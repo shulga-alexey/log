52179405c06ed3b23f147f0a0c769fce94410183
Алексей Шульга <shulga.alexey.00@yandex.ru>
2023-05-13 20:39:10 +0300

    Creation of the second versions of the tasks.models; users app removed.

--- log_project/log_project/settings.py
+++ log_project/log_project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
-    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
--- log_project/tasks/models.py
+++ log_project/tasks/models.py
@@ -1,7 +1,35 @@
+from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
-from users.models import Teacher, Student
+
+class Teacher(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    tasks = models.ManyToManyField('Task',
+                                   through='AppointedTask',
+                                   through_fields=('student', 'task'))
+    group = models.ForeignKey('StudentsGroup',
+                              on_delete=models.SET_NULL,
+                              related_name='students',
+                              blank=True,
+                              null=True)
+
+
+class StudentsGroup(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
 
 
 class Task(models.Model):
@@ -13,15 +41,15 @@ class Task(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     text = models.TextField()
-    teacher = models.ForeignKey(Teacher,
-                                on_delete=models.CASCADE,
-                                related_name='tasks')
-    students = models.ManyToManyField(Student,
-                                      related_name='tasks')
+    producer_teacher = models.ForeignKey('Teacher',
+                                         on_delete=models.CASCADE,
+                                         related_name='tasks')
+    students = models.ManyToManyField('Student',
+                                      through='AppointedTask',
+                                      through_fields=('task', 'student'))
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.REJECTED)
-    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -30,3 +58,14 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class AppointedTask(models.Model):
+    task = models.ForeignKey('Task',
+                             on_delete=models.CASCADE,
+                             related_name='appointed')
+    student = models.ForeignKey('Student',
+                                on_delete=models.CASCADE)
+    appointor_teacher = models.ForeignKey(Teacher,
+                                          on_delete=models.CASCADE)
+    date = models.DateTimeField()
